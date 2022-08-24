8f12538b3e2a23656cea7b1b4c16fcd169db6bcf
Jony198207 <101894380+Jony198207@users.noreply.github.com>
2022-08-24 16:46:05 -0500

    Update P2A3-Semaforo.ino

--- Practica02/P2A3-Semaforo.ino
+++ Practica02/P2A3-Semaforo.ino
@@ -1,40 +1,47 @@
-#define LED1 13
-#define LED2 12
-#define LED3 11
-#define LED4 10
-#define LED5 9
-#define LED6 8
+// Nota: se habla indistintamente entre foco y LED.
+
+#define LED1 13 // Define el puerto 13 como la señal del LED 1 (Foco rojo del primer semaforo)
+#define LED2 12 // Define el puerto 12 como la señal del LED 2 (Foco amarillo del primer semaforo)
+#define LED3 11 // Define el puerto 11 como la señal del LED 3 (Foco verde del primer semaforo)
+#define LED4 10 // Define el puerto 10 como la señal del LED 4 (Foco rojo del segundo semaforo)
+#define LED5 9 // Define el puerto 9 como la señal del LED 5 (Foco amarillo del segundo semaforo)
+#define LED6 8 // Define el puerto 8 como la señal del LED 6 (Foco verde del segundo semaforo)
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(LED1,OUTPUT);
-  pinMode(LED2,OUTPUT);
-  pinMode(LED3,OUTPUT);
-  pinMode(LED4,OUTPUT);
-  pinMode(LED5,OUTPUT);
-  pinMode(LED6,OUTPUT);
+  // Espacio en el que se definen entradas y salidas. 
+  pinMode(LED1,OUTPUT); // Define LED1 como salida 
+  pinMode(LED2,OUTPUT); // Define LED2 como salida 
+  pinMode(LED3,OUTPUT); // Define LED3 como salida 
+  pinMode(LED4,OUTPUT); // Define LED4 como salida 
+  pinMode(LED5,OUTPUT); // Define LED5 como salida 
+  pinMode(LED6,OUTPUT); // Define LED6 como salida 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(LED1,HIGH);
-  digitalWrite(LED6,HIGH);
-  delay(5000);
+  // Loop para ejecutar el código mientras se este activo
   
-  digitalWrite(LED6,LOW);
-  digitalWrite(LED5,HIGH);
-  delay(1000);
-  digitalWrite(LED1,LOW);
-  digitalWrite(LED5,LOW);
+  digitalWrite(LED1,HIGH); // Enciende el foco rojo del primer semaforo 
+  digitalWrite(LED6,HIGH); // Enciende el foco verde del segundo semaforo
+  delay(5000); // da delay de 5 seg. para que los focos anteriores duren encendidos este tiempo antes de la siguiente instruccion 
+  
+  digitalWrite(LED6,LOW); // Apaga el foco verde del segundo semaforo
+  digitalWrite(LED5,HIGH); // Prende el foco amarillo del segundo semaforo
+  delay(1000); // delay de 1 segundo para que el foco amarillo del segundo semaforo dure este tiempo prendido
+  digitalWrite(LED1,LOW); // Apaga foco rojo del primer semaforo 
+  digitalWrite(LED5,LOW); // Apaga el foco amarillo del segundo semaforo
 
-  digitalWrite(LED3,HIGH);
-  digitalWrite(LED4,HIGH);
-  delay(5000);
+  digitalWrite(LED3,HIGH); // Prende el foco verde del primer semaforo
+  digitalWrite(LED4,HIGH); // Prende el foco rojo del segundo semaforo
+  delay(5000); // Da tiempo de 5 segundos para que las dos instrucciones anteriores duren este tiempo antes de ejecutar la siguiente instruccion
 
-  digitalWrite(LED3,LOW);
-  digitalWrite(LED2,HIGH);
-  delay(1000);
-  digitalWrite(LED4,LOW);
-  digitalWrite(LED2,LOW);
+  digitalWrite(LED3,LOW); // Apaga el foco verde del primer semaforo
+  digitalWrite(LED2,HIGH); // Prende el foco amarillo del primer semaforo
+  delay(1000); // Da un tiempo de un segundo para que dure encendido el foco amarillo anterior
+  digitalWrite(LED4,LOW); // Apaga el foco rojo del segundo semaforo
+  digitalWrite(LED2,LOW); // Apaga el foco amarillo del primer semaforo
+  
+  // Aqui ya se cumplio la secuencia simultanea de los dos semaforos que se pedia, si el programa sigue corriendo en el arduino, lo anterior se repite debido al Loop.
 
 }
