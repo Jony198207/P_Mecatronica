2c535cc37a367c9c9d07db03a3fb9c17a07958ed
Jony198207 <101894380+Jony198207@users.noreply.github.com>
2022-09-02 15:04:07 -0500

    Create P4A1-NombresLCD.ino

---
+++ Práctica04/P4A1-NombresLCD.ino
@@ -0,0 +1,20 @@
+# include <LiquidCrystal.h> // importamos o incluimos librerias.
+
+LiquidCrystal lcd(12,11,5,4,3,2); // Definimos los pines que usará el lcd
+int indice = 15; // establecemos la totalidad de columnas en un fila de lcd. 
+void setup() {
+  lcd.begin(16,2); // Definimos el lcd de una pantalla 16x2
+}
+
+void loop() {
+  lcd.setCursor(indice,0); // Definimos que el cursor en la pantalla empezará en la fila y columna 0.
+  lcd.print("Jonathan_G      "); // imprime en pantalla el nombre de un integrante del equipo. Se dejan 6 espacios en blanco para completar los 16.
+  lcd.setCursor(indice,1); // Definimos que el cursor en la pantalla empezará en la fila 1 y columna 4.
+  lcd.print("Dante_F         "); // imprime en pantalla el nombre de un integrante del equipo.Se dejan 6 espacios en blanco para completar los 19.
+
+  // En este if se va reduciendo, para recorrer los nombres de derecha a izquierda
+  if (indice > 0){
+    indice = indice - 1;
+  }
+  delay(100); // Retardo de 0.1 seg.
+}
