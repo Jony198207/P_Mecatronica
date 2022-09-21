6cd9ef0ae43c8ba5ffaf9562125b1f65c416ecb8
Jony198207 <101894380+Jony198207@users.noreply.github.com>
2022-09-20 20:47:22 -0500

    Update P4A5-SistemaEsclavoEjercicioEjemplo.ino

--- Práctica04/P4A5-SistemaEsclavoEjercicioEjemplo.ino
+++ Práctica04/P4A5-SistemaEsclavoEjercicioEjemplo.ino
@@ -1,28 +1,28 @@
 # include <Wire.h>
 
-#define led 13;
+#define led 13; // Puerto 13 asociado a un led
 
-int dato; 
+int dato; //variable entera para recibir el dato de la comunicacion 
 
 void setup() {
-  Wire.begin(0xAF);
-  Wire.onReceive(lectura);
-  pinMode(led,OUTPUT);
-  Serial.begin(9600);
+  Wire.begin(0xAF); // Empieza comunicacion usando la clave acordada (0xAF)
+  Wire.onReceive(lectura); // Determina que va a recibir informacion por medio de la funcion lectura
+  pinMode(led,OUTPUT); // Define el pin 13 como salida 
+  Serial.begin(9600); // Comunicacion a 9600 baudios 
 }
 
 void loop() {
-  Serial.println(dato);
-  delay(100);
+  Serial.println(dato); // Imprime el dato obtenido desde el sistema maestro
+  delay(100); // Retardo de .1 seg.
 
 }
 
 void lectura() {
-  dato = Wire.read();
-  if (dato == 1) {
-    digitalWrite(led,HIGH);
+  dato = Wire.read(); // Asigna a la variable dato previamente definida el contenido de leer lo que envia el sistema maestro
+  if (dato == 1) { // condicional 
+    digitalWrite(led,HIGH); // Si llega a este punto, prende el led
   }
   else {
-    digitalWrite(led,LOW);
+    digitalWrite(led,LOW); // Si el dato no es 1 (que en este caso entonces sería 0), el foco se apaga.
   }
 }
