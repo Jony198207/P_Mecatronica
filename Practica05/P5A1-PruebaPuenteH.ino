3ee75a4d6941d981c78ba4cb7ab6b96a3eb0e3d8
Jony198207 <101894380+Jony198207@users.noreply.github.com>
2022-09-28 20:04:35 -0500

    Update P5A1-PruebaPuenteH.ino

--- Practica05/P5A1-PruebaPuenteH.ino
+++ Practica05/P5A1-PruebaPuenteH.ino
@@ -11,18 +11,25 @@ void setup() {
 }
 
 void loop() {
+  // led del pin 10 encendido
   digitalWrite(MotA,HIGH); // Damos estado alto al pin 10
   digitalWrite(MotB,LOW); // Estado bajo del pin 11
   delay(1000); // Retardo de 1 segundo mientras se realiza un levogiro
   Serial.print("Levogiro"); // Impresion textual
+  
+  // ambos leds apagados
   digitalWrite(MotA,LOW); // Estado bajo "0" del pin 10
   digitalWrite(MotB,LOW); // Estado bajo del pin 11
   delay(1000); // Retardo de 1 segundo
   Serial.print("Freno pasivo"); // Imprime Freno pasivo, pues no hay movimiento en este punto
+  
+  // Led del pin 11 prendido
   digitalWrite(MotA,LOW); // Estado bajo del pin 10
   digitalWrite(MotB,HIGH); // Estado alto del pin 11
   delay(1000); // Retardo de 1 segundo
   Serial.print("Dextrogiro"); // Se hace dextrogiro y lo imprime en la comunicación serial
+  
+  // ambos leds prendidos
   digitalWrite(MotA,HIGH); // Estado alto del pin 10
   digitalWrite(MotB,HIGH); // Estado bajo del pin 11 
   delay(1000); // Retardo de 1 segundo
