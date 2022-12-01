6555dc75d39e1c7d0a56b83b21f095464bc230e8
Jony198207 <101894380+Jony198207@users.noreply.github.com>
2022-11-30 20:15:45 -0600

    Update P10A2-MaqEdos.ino

--- Practica10/P10A2-MaqEdos.ino
+++ Practica10/P10A2-MaqEdos.ino
@@ -1,40 +1,45 @@
-# define LED 13
+# define LED 13 // Led de la placa Arduino
 
 int estado;
-int light_dif = 25;
+int light_dif = 25; // Valor dado para calar el funcionamiento de la maquina de estados
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(LED, OUTPUT);
+  pinMode(LED, OUTPUT); // Definimos el pin como salida
   
   Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  
+  // Switch para contemplar casos
   switch(estado){
+    // Caso 0, que nos manda directamente al estado 1 después de imprimir "Estado 0"
     case 0:
       Serial.println("Estado 0");
       estado = 1;
-      break;
-      
+      break; // termina el caso 0
+     
+     // Caso 1, donde apagamos el led y si light_dif es menor a 30, manda a estado 2
      case 1:
       Serial.println("Estado 1");
       digitalWrite(LED, LOW);
       if (abs(light_dif) < 30){
         estado = 2;
       }
-      else{
+      else{ // Si no se cumple la condicion, regresamos al estado 0
         estado = 0;
       }
-      break;
+      break; // termina el caso 1
+    
+    // Caso 2, prendemos el led e imprimimos "Estado 2", finalmente regresamos al estado 0
     case 2:
       Serial.println("Estado 2");
       digitalWrite(LED, HIGH);
       delay(1000);
       estado = 0;
-      break;
+      break; // termina el caso 2
     
   
   }
